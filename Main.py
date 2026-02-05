3f4566483ed60918f4761d4bc3d9c6b9f7fe4beb
JHerrerat7 <jherrerat4@ucentral.edu.co>
2026-02-04 23:34:24 -0500

    Envio Via Correo Electronico

---
+++ Main.py
@@ -0,0 +1,45 @@
+import yagmail
+
+class EmailService:
+    def __init__(self, correo, password):
+        self.yag = yagmail.SMTP(correo, password)
+
+    def enviar(self, destinatarios, asunto, cuerpo):
+        self.yag.send(
+            to=destinatarios,
+            subject=asunto,
+            contents=cuerpo
+        )
+
+
+def construir_mensaje(integrantes, repo_url):
+    mensaje = "Integrantes del equipo:\n"
+    for integrante in integrantes:
+        mensaje += f"- {integrante}\n"
+
+    mensaje += f"\nRepositorio del proyecto:\n{repo_url}"
+    return mensaje
+
+
+if __name__ == "__main__":
+    correo = "jherrerat4@ucentral.edu.co"
+    password_app = "droe bxko cjnn vvnl"
+
+    integrantes = [
+        "Jean Marco Herrera Torres",
+        "Nelson Nicolas Cruz Capella"
+    ]
+
+    repo_url = "https://github.com/JHerrerat7/Envi-de-Correo-v-a-Gmail--Python.git"
+    destinatarios = ["jeangamerf@gmail.com"]
+
+    servicio = EmailService(correo, password_app)
+    mensaje = construir_mensaje(integrantes, repo_url)
+
+    servicio.enviar(
+        destinatarios,
+        "Entrega ejercicio – Envío de correo",
+        mensaje
+    )
+
+    print("Correo enviado correctamente")
